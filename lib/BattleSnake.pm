254bef9d49661093fa76ed31087cf47eaf351ca9
rawleyfowler <rawleyfowler@proton.me>
2024-10-30 12:43:01 -0500

    initial commit

---
+++ lib/BattleSnake.pm
@@ -0,0 +1,13 @@
+package BattleSnake;
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+use Carp ();
+
+sub move {
+    Carp::croak('TODO: IMPLEMENT ME');
+}
+
+1;
---
+++ snake.pl
@@ -0,0 +1,37 @@
+use Mojolicious::Lite;
+use Mojo::File qw(curfile);
+use lib curfile->sibling('lib')->to_string();
+use BattleSnake;
+
+our $VERSION = '1';
+
+post '/move' => sub {
+    my $c     = shift;
+    my $board = $c->req->json;
+    my $move  = BattleSnake->move($board);
+    $c->render( json => $move );
+};
+
+get '/' => sub {
+    shift->render(
+        json => +{
+            apiversion => $VERSION,
+            author     => 'rawleyfowler & gabrielgavrilov',
+            color      => '#0f0faa',
+            head       => 'default',
+            tail       => 'default'
+        }
+    );
+};
+
+get '/end' => sub {
+    app->log->info('Game over...');
+    shift->render( json => +{ good => 'game' } );
+};
+
+post '/start' => sub {
+    app->log->info('Starting...');
+    shift->render( json => +{ lets => 'do this' } );
+};
+
+app->start;
